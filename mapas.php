a084d59999e2acf1194d69b328dec5572afccf3d
Cambapo <31019667+Cambapo@users.noreply.github.com>
2017-09-03 19:29:16 -0500

    Update mapas.php

--- mapas.php
+++ mapas.php
@@ -1,9 +1,7 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
   <head>
   <meta charset="utf-8">
-
-
     <title> Web Tracking </title>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="icon" href="./images/syrus.ico">
@@ -11,87 +9,73 @@
       <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
   </head>
   <body>
-      <meta http-equiv="refresh" content="5">
+      <meta http-equiv="refresh" content="15">
   </body>
+    <?php
 
-            <?php
+      include 'database.php';
 
+      $query = "SELECT * FROM locations WHERE 1 ";
 
-                $servername = "designlocations.cl8waza61otc.us-east-2.rds.amazonaws.com";
-                $username = "abcr";
-                $password = "abcr1234";
-                // Create connection
-                $conn = new mysqli($servername, $username, $password);
+      $row=connection2rds($query);
 
-                // Check connection
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
-                #echo "Connected successfully";
-                mysqli_select_db($conn, "designlocations");
-                #$query = 'select * from gatos WHERE Nombre LIKE '%" . $name .  "%' OR Patrón LIKE '%" . $name ."%'";';
-                $query = "SELECT * FROM locations WHERE 1 ";
+      $Id = $row[0];
+      $Lat = $row[1];
+      $Long = $row[2];
+      $Date=$row[3];
+      $Time=$row[4];
 
-                $result = mysqli_query($conn, $query);
-                $finfo = mysqli_fetch_field($result);
-                mysqli_data_seek($result, 1);
-                $row = mysqli_fetch_row($result);
+      if ($Lat == 0 and $Long == 0) {
+            
+        echo "<p> GPS NO CONECTADO </p>";
+        
+      } else {
+       
+        echo "<li>";
+        print "ID: $Id";
+        echo "<br>";
+        echo "<li>";
+        print "Latitud: $Lat";
+        echo "<br>";
+        echo "<li>";
+        print "Longitud: $Long";
+        echo "<br>";
+        echo "<li>";
+        print "Date: $Date";
+        echo "<li>";
+        print "Time: $Time";
+        echo "<br>";
+      
+      }
+    ?>
 
-                while ($row = mysqli_fetch_array($result)) {
+  <style>
+    #map {
+      height: 500px;
+      width: 100%;
+    }
+  </style>
 
+  <div id="map"></div>
+  <script>
 
-                    $Id = $row['ID'];
-                    $Lat = $row['Latitude'];
-                    $Long = $row['Longitude'];
-                    $Date=$row['Date'];
-                    $Time=$row['Time'];
+  var lat = "<?php echo $Lat; ?>";
+  var lon = "<?php echo $Long; ?>";
+  var myLatLng = {lat: parseFloat(lat), lng: parseFloat(lon)};
 
+  function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {zoom: 16, center: myLatLng,});
+    var marker = new google.maps.Marker({ position: myLatLng,map: map, title:'Su auto'});
+  }
 
+  </script>
+  <script async defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCp2b5o90_5K1NbK5qZj86P6Hn61xhUFII&callback=initMap">
+  </script>
 
-                  }
+  <h1 class="red-text ubuntu title">Las coordenadas del vehículo son: </h1>
+  <form action="historicotest.php">
+  <input type="submit" value="Historico" class="dropbtn">
+  </form>
 
-
-            ?>
-
-            <style>
-              #map {
-                height: 500px;
-                width: 100%;
-               }
-            </style>
-
-                <div id="map"></div>
-                <script>
-
-
-                var lat = "<?php echo $Lat; ?>";
-                var lon = "<?php echo $Long; ?>";
-                var myLatLng = {lat: parseFloat(lat), lng: parseFloat(lon)};
-
-
-                    function initMap() {
-                    var map = new google.maps.Map(document.getElementById('map'), {
-                      zoom: 16,
-                      center: myLatLng,
-                    });
-                    var marker = new google.maps.Marker({
-                      position: myLatLng,
-                      map: map,
-                      title:'Su auto'
-                    });
-                  }
-
-                </script>
-                <script async defer
-                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCp2b5o90_5K1NbK5qZj86P6Hn61xhUFII&callback=initMap">
-                </script>
-
-                  <h1 class="red-text ubuntu title">Las coordenadas del vehículo son: </h1>
-
-                      <?php include 'database.php';?>
-
-                      <form action="historico.php">
-                      <input type="submit" value="Historico" class="dropbtn">
-                      </form>
-
-                      </html>
+</html>
