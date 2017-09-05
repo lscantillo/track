ea192c1fed369802e38422c472210d903296ae99
lscantillo <30596335+lscantillo@users.noreply.github.com>
2017-09-04 22:38:44 -0500

    Update mapas.php

--- mapas.php
+++ mapas.php
@@ -74,8 +74,6 @@
   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCp2b5o90_5K1NbK5qZj86P6Hn61xhUFII&callback=initMap">
   </script>
   
-  <form action="historico.php">
-  <input type="submit" value="Historico" class="dropbtn">
-  </form>
+  
 
 </html>
