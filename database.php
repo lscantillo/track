a6c0f7f132fb65049573ec0859d29a248f9e6c1f
lscantillo <30596335+lscantillo@users.noreply.github.com>
2017-09-10 15:55:50 -0500

    Update database.php

--- database.php
+++ database.php
@@ -51,11 +51,11 @@
         }
         
         if($hb>=0 AND ($he!=0 AND $hb!=0)){
-            $hbg=" Time> ".$hb.":00:00 ";
+            $hbg=" Time> ".$hb.":00 ";
         }
 
         if($he>=0 AND ($he!=0 AND $hb!=0)){
-            $hen=" Time< ".$he.":00:00 ";
+            $hen=" Time< ".$he.":00 ";
         }
 
         if($limit>0){
