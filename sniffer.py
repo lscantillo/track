0fd40ed54bae55f3152a1062978df1206b933c59
lscantillo <30596335+lscantillo@users.noreply.github.com>
2017-09-10 17:29:26 -0500

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -11,13 +11,14 @@ def main():
     print("Connecting to MySQL DB.")
     db = MySQLdb.connect('designlocations.cl8waza61otc.us-east-2.rds.amazonaws.com','abcr','abcr1234','designlocations')
     cursor = db.cursor()
-
+    print("Retrieving latest database data")
+    startid = SELECT id FROM locations ORDER BY id DESC LIMIT 0, 1
     print("Creating socket object.")
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     print("Defining socket (ip+port).")
     HOST = socket.gethostbyname(socket.gethostname())
     s.bind((HOST, 56303))
-    print("Arigato ningen.")
+    print("Looks good for now!")
     print("Accepting.")
     while True:
         while True:
@@ -28,9 +29,9 @@ def main():
                 itIs, EventDef, seco, Year, Month, Mnum, Day, Hour, Minutes, Seconds, lat, lon = getMess(info)
                 if itIs == 1:
                     print("Event " + str(EventDef) + " was triggered, the latitude is " + str(lat) + " and longitude is " + str(lon))
-                    date=str(Year)+"-"+str(Mnum)+"-"+str(Day)
-                    time=str(Hour)+":"+str(Minutes)+":"+str(Seconds)
-                    cursor.execute("""INSERT INTO designlocations.locations (ID, Latitude, Longitude, Date, Time) VALUES (%s, %s, %s, %s, %s)""",(EventDef, lat, lon, date, time))
+                    datetime=str(Year)+"-"+str(Mnum)+"-"+str(Day)+" "+str(Hour)+":"+str(Minutes)+":"+str(Seconds)
+                    print("It was triggered at time "+datetime)
+                    cursor.execute("""INSERT INTO designlocations.locations (ID, Latitude, Longitude, DateTime) VALUES (%s, %s, %s, %s)""",(EventDef, lat, lon, datetime))
 
                     db.commit()
                 else:
@@ -48,7 +49,8 @@ def getMess(m):
         # Confirmation
         itIs = 1
         # Event
-        EventDef = int(m[4:6])
+        startid = startid + 1
+        EventDef = startid
         # Time
         secn, Year, Month, Mnum, Day, Hour, Minutes, Seconds = getTime(int(m[6:10]), int(m[10]), int(m[11:16]))
         # Coordinates
