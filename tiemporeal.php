eaa8558b99865ea8fd3f02aeea024d000594e600
lscantillo <30596335+lscantillo@users.noreply.github.com>
2017-09-17 13:21:55 -0500

    Update tiemporeal.php

--- tiemporeal.php
+++ tiemporeal.php
@@ -41,6 +41,7 @@
 
      var lat = "<?php echo $Lat; ?>";
      var lon = "<?php echo $Long; ?>";
+     var myPath = [];
      var image = 'https://cdn0.iconfinder.com/data/icons/isometric-city-basic-transport/48/truck-front-01-48.png';
        function initMap() {
          var myLatLng = {lat: parseFloat(lat), lng: parseFloat(lon)};
