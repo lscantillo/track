03585bc39355138daf2bdb97799ef8ada02fa54c
lscantillo <30596335+lscantillo@users.noreply.github.com>
2017-09-18 01:17:22 -0500

    Update database.php

--- database.php
+++ database.php
@@ -34,12 +34,13 @@
         return $answer;
     }
 
-    function suscintquery ($fstdt,$scnddt,$hb,$he,$limit){
+    function suscintquery ($fstdt,$scnddt,$hb,$he,$place,$limit){
         $query="SELECT * FROM locations";
         $ybg="";
         $yen="";
         $hbg="";
         $hen="";
+        $plc="";
         $lmt="";
         
         if(strlen($fstdt)>0){
@@ -62,6 +63,16 @@
         
         if($limit>0){
             $lmt="LIMIT ".$limit;
+        }
+        
+        if (strlen($place)>0){
+            $array = lookup($place);
+            $plc="Latitude BETWEEN ".($array['latitude']-0.002)." AND ".($array['latitude']+0.002)." ";
+            $plc=$plc."AND Longitude BETWEEN ".($array['longitude']-0.002)." AND ".($array['longitude']+0.002);
+        }
+        
+        if(strlen($plc)>0){
+            $query=whereand($query,$plc);
         }
         
         if(strlen($ybg)>0){
@@ -91,6 +102,37 @@
         }else{
             return $query;
         }
+    }
+
+    function lookup($string){
+ 
+        $string = str_replace (" ", "+", urlencode($string));
+        $details_url = "http://maps.googleapis.com/maps/api/geocode/json?address=".$string."&sensor=false";
+ 
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $details_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = json_decode(curl_exec($ch), true);
+ 
+        // If Status Code is ZERO_RESULTS, OVER_QUERY_LIMIT, REQUEST_DENIED or INVALID_REQUEST
+        if ($response['status'] != 'OK') {
+            return null;
+        }
+ 
+        //print_r($response);
+        $geometry = $response['results'][0]['geometry'];
+ 
+        $longitude = $geometry['location']['lat'];
+        $latitude = $geometry['location']['lng'];
+ 
+        $array = array(
+            'latitude' => $geometry['location']['lng'],
+            'longitude' => $geometry['location']['lat'],
+            'location_type' => $geometry['location_type'],
+        );
+ 
+        return $array;
+ 
     }
 
     function querygenerator($yb,$ye,$mb,$me,$db,$de,$hb,$he,$limit)
