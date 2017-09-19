362caed6b5297e062863a630aed3afff87310e3e
lscantillo <30596335+lscantillo@users.noreply.github.com>
2017-09-19 11:58:47 -0500

    Update karte.php

--- karte.php
+++ karte.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    //<link rel="stylesheet" href="/resources/demos/style.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
     <link rel="stylesheet" href="/time/jquery-timepicker-master/jquery.timepicker.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -68,7 +68,7 @@
 </head>
 
   <body>
-    <form action="finalhistoric.php" name="StartDate" method="POST">
+    <form action="historicquery.php" name="StartDate" method="POST">
       <p>Fecha incial: <input name="date1" type="text" id="dtstart"></p>
       <p>Hora inicial: <input name="time1" type="text" id="tmstart"></p>
       <p>Fecha final: <input name="date2" type="text" id="dtend"></p>
