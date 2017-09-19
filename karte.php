fda0743d93058b1fed57252ffb6bd81c502d7bce
lscantillo <30596335+lscantillo@users.noreply.github.com>
2017-09-19 11:56:05 -0500

    Update karte.php

--- karte.php
+++ karte.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
+    //<link rel="stylesheet" href="/resources/demos/style.css">
     <link rel="stylesheet" href="/time/jquery-timepicker-master/jquery.timepicker.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
