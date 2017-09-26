95bfbf9f5c9a288126becaa53e7410ceb33952ee
lscantillo <30596335+lscantillo@users.noreply.github.com>
2017-09-25 21:38:16 -0500

    Update finalhistoric.php

--- finalhistoric.php
+++ finalhistoric.php
@@ -4,77 +4,109 @@
     <meta charset="utf-8">
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:700italic,400,300,700' rel='stylesheet' type='text/css'>
+    <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+    <script src="js/skel.min.js"></script>
+    <script src="js/skel-panels.min.js"></script>
+    <script src="js/init.js"></script>
+    <noscript>
+      <link rel="stylesheet" href="css/skel-noscript.css" />
+      <link rel="stylesheet" href="css/style.css" />
+      <link rel="stylesheet" href="css/style-desktop.css" />
+    </noscript>
 
-    <title></title>
+    <title>ABCR Design: Filtrado histórico</title>
   </head>
   <?php include 'finalquery.php' ?>
-<div >
 
-</div>
   <body>
+   <div id="header">
+      <div class="container">
 
-        <style>
-      #map {
-        height: 80%;
-      }
-      html, body {
-        height: 80%;
-        margin: 20;
-        padding: 20;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="map"></div>
+        <!-- Logo -->
+          <div id="logo">
+            <h1><a href="#">a b c r design</a></h1>
+          </div>
+
+        <!-- Nav -->
+          <nav id="nav">
+            <ul>
+              <li><a href="index.php">Rastreo</a></li>
+              <li class="active"><a href="historico.php">Histórico</a></li>
+            </ul>
+          </nav>
+
+      </div>
+    </div>
+  <!-- Header -->
+
+  <!-- Banner -->
+  <div id="map"></div>
+  <div id="filtercontrols">
+
+              <form action=historico.php>
+              <input type="submit" class= "apply" name="submit" value="Retornar">
+              </form>
+
+    </div>
     <script>
     var id = "<?php echo $Id; ?>";
     var lat = "<?php echo $Lat; ?>";
     var lon = "<?php echo $Long; ?>";
     var myPath = [];
     var image = 'https://cdn0.iconfinder.com/data/icons/isometric-city-basic-transport/48/truck-front-01-48.png';
-      <?php
-            echo "var markers=$markers";
-        ?>
-
     function initMap() {
-      var myLatLng = {lat: parseFloat(lat), lng: parseFloat(lon)};
-      var myOptions = {
-          zoom: 10,
-          center: myLatLng,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          mapTypeControl: false
-      };
-
-      var map = new google.maps.Map(document.getElementById('map'),myOptions);
-      var infowindow = new google.maps.InfoWindow(), marker, lat, lng;
-      var json=JSON.parse( markers );
-
-      for( var o in json ){
-
-          lat = json[ o ].Latitude;
-          lng=json[ o ].Longitude;
-          name=json[ o ].ID;
-
-          marker = new google.maps.Marker({
-              position: new google.maps.LatLng(lat,lng),
-              name:name,
-              map: map
-          });
-          google.maps.event.addListener( marker, 'click', function(e){
-              infowindow.setContent( this.name );
-              infowindow.open( map, this );
-          }.bind( marker ) );
-      }
+            var myLatLng = {lat: parseFloat(lat), lng: parseFloat(lon)};
+             var myOptions = {
+                 zoom: 16,
+                 center: myLatLng,
+                 panControl: true,
+                 zoomControl: true,
+                 scaleControl: true,
+                 mapTypeId: google.maps.MapTypeId.ROADMAP
+             }
+          map = new google.maps.Map(document.getElementById("map"), myOptions);
+  //  setInterval(function mapload(){
+       $(document).ready(function() {
+          $.ajax({
+                 url: "finalquery.php",
+                 // data: form_data,
+                success: function(hist)
+                {
+                    var json_hist = jQuery.parseJSON(JSON.stringify(hist));
+                    INIT_LAT = parseFloat(json_hist[json_hist.length - 1].Latitude);
+                    INIT_LON = parseFloat(json_hist[json_hist.length - 1].Longitude);
+                    $(json_hist).each(function() {
+                      var ID = this.ID;
+                      var LATITUDE = this.Latitude;
+                      var LONGITUDE = this.Longitude;
+                      myCoord2 = new google.maps.LatLng(parseFloat(LATITUDE), parseFloat(LONGITUDE));
+                      myPath.push(myCoord2);
+                      var myPathTotal2 = new google.maps.Polyline({
+                        path: myPath,
+                        strokeColor: '#e95d3c',
+                        strokeOpacity: 1.0,
+                        strokeWeight: 5
+                      });
+                      myPathTotal2.setPath(myPath)
+                      myPathTotal2.setMap(map);
+                      addMarker(new google.maps.LatLng(LATITUDE, LONGITUDE), map);
+                    });
+                },
+                dataType: "json"//set to JSON
+              })
+    });
   }
-        // function addMarker(latLng, map) {
-        //            var marker = new google.maps.Marker({
-        //                position: latLng,
-        //                map: map,
-        //                icon: image
-        //            });
-        //            return marker;
-        //       }
+        function addMarker(latLng, map) {
+                   var marker = new google.maps.Marker({
+                       position: latLng,
+                       map: map,
+                       icon: image
+                   });
+                   return marker;
+              }
     </script>
 
     <script async defer
@@ -83,4 +115,3 @@
 
   </body>
 </html>
-
