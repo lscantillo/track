001c4fd765939052dbe6f2a04264aadc266eaab0
lscantillo <30596335+lscantillo@users.noreply.github.com>
2017-09-26 14:16:20 -0500

    Create finalhistoric2.php

---
+++ finalhistoric2.php
@@ -0,0 +1,32 @@
+<?php
+                    include 'database.php';
+                    $servername = "designlocations.cl8waza61otc.us-east-2.rds.amazonaws.com";
+                    $username = "abcr";
+                    $password = "abcr1234";
+                          // Create connection
+                    $conn = new mysqli($servername, $username, $password);
+                          // Check connection
+                    if ($conn->connect_error) {
+                      die("Connection failed: " . $conn->connect_error);
+                    }
+                           #echo "Connected successfully";
+                    mysqli_select_db($conn, "designlocations");
+                    $query=suscintquery($_POST['datetimepicker'],$_POST['datetimepicker2'],$_POST["plc"]);
+
+                    $result = mysqli_query($conn, $query);
+
+                   $hist=[];
+                  //$hist= array();
+                    while($row = mysqli_fetch_array($result))
+                    {
+                        $Id = $row['ID'];
+                        $Lat = $row['Latitude'];
+                        $Long = $row['Longitude'];
+                        $hist[]=$row;
+
+                    }
+
+                   // $markers= json_encode($hist);
+                   $historico=json_encode($hist);
+
+                  ?>
