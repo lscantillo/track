371b70934ce2c614cd22f9112ece89223ea43f46
lscantillo <30596335+lscantillo@users.noreply.github.com>
2017-09-26 14:22:00 -0500

    Update finalhistoric.php

--- finalhistoric.php
+++ finalhistoric.php
@@ -85,43 +85,12 @@
   //  setInterval(function mapload(){
       //  $(document).ready(function() {
           $.ajax({
-                 url: "finalquery2.php",
+                 url: "finalquery.php",
                  // data: form_data,
                 success: function(hist)
                 {
-                  <?php
-                    include 'database.php';
-                    $servername = "designlocations.cl8waza61otc.us-east-2.rds.amazonaws.com";
-                    $username = "abcr";
-                    $password = "abcr1234";
-                          // Create connection
-                    $conn = new mysqli($servername, $username, $password);
-                          // Check connection
-                    if ($conn->connect_error) {
-                      die("Connection failed: " . $conn->connect_error);
-                    }
-                           #echo "Connected successfully";
-                    mysqli_select_db($conn, "designlocations");
-                    $query=suscintquery($_POST['datetimepicker'],$_POST['datetimepicker2'],$_POST["plc"]);
-
-                    $result = mysqli_query($conn, $query);
-
-                   $hist=[];
-                  //$hist= array();
-                    while($row = mysqli_fetch_array($result))
-                    {
-                        $Id = $row['ID'];
-                        $Lat = $row['Latitude'];
-                        $Long = $row['Longitude'];
-                        $hist[]=$row;
-
-                    }
-
-                   // $markers= json_encode($hist);
-                   $historico=json_encode($hist);
-
-                  ?>
-                    var json_hist = jQuery.parseJSON(JSON.stringify(historico));
+                  
+                    var json_hist = jQuery.parseJSON(JSON.stringify(hist));
                     initMap();
                     INIT_LAT = parseFloat(json_hist[json_hist.length - 1].Latitude);
                     INIT_LON = parseFloat(json_hist[json_hist.length - 1].Longitude);
