2791eafafbcd48614afadda053abf4444fc7df3b
Luis S Cantillo <30596335+lscantillo@users.noreply.github.com>
2017-10-16 18:23:45 -0500

    Update finalquery.php

--- finalquery.php
+++ finalquery.php
@@ -11,7 +11,42 @@
   }
          #echo "Connected successfully";
   mysqli_select_db($conn, "designlocations");
-  $query=suscintquery("locations",$_POST['datetimepicker'],$_POST['datetimepicker2'],$_POST["plc"]);
+  function suscintquery ($fstdt,$scnddt,$place){
+        $query="SELECT * FROM locations";
+        $ybg="";
+        $yen="";
+        $plc="";
+        
+        if(strlen($fstdt)>0){
+            $ybg="DateTime >= '".$fstdt;
+        }
+
+        if(strlen($scnddt)>0){
+            $yen="DateTime <= '".$scnddt;
+        }
+        
+        if (strlen($place)>0){
+            $array = lookup($place);
+            $plc="Latitude BETWEEN ".($array['latitude']-0.004)." AND ".($array['latitude']+0.004);
+            $plc=$plc." AND Longitude BETWEEN ".($array['longitude']-0.004)." AND ".($array['longitude']+0.004);
+        }
+        
+        if(strlen($plc)>0){
+            $query=whereand($query,$plc);
+        }
+        
+        if(strlen($ybg)>0){
+            $query=whereand($query,$ybg);
+        }
+
+        if(strlen($yen)>0){
+            $query=whereand($query,$yen);
+        }
+
+        return $query;
+
+    }
+  $query=suscintquery($_POST['datetimepicker'],$_POST['datetimepicker2'],$_POST["plc"]);
 
   $result = mysqli_query($conn, $query);
 
