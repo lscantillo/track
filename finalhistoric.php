d4147ef7e7d0d4f46854ff09cd7670ef97412a89
Luis S Cantillo <30596335+lscantillo@users.noreply.github.com>
2017-10-17 11:37:09 -0500

    tried to fix historic to display variable hist2

--- finalhistoric.php
+++ finalhistoric.php
@@ -20,7 +20,7 @@
 
     <title>ABCR Design: Filtrado histórico</title>
   </head>
-    <?php include 'finalquery2.php' ?>
+    <!-- <?php include 'finalquery2.php' ?> -->
   <body>
    <div id="header">
       <div class="container">
@@ -59,9 +59,13 @@
     var lat = "48.85809";
     var lon = "2.294694";
     var myPath = [];
+    var myPath2 = [];
     infoWindows = Array();
+    infoWindows2 = Array();
     markers = Array();
+    markers2 = Array();
     var image = 'https://cdn0.iconfinder.com/data/icons/isometric-city-basic-transport/48/truck-front-01-48.png';
+    var image2 = 'https://i.imgur.com/FGEuaWh.png';
     function initMap() {
              
              var myOptions = {
@@ -81,11 +85,25 @@
                        icon: image,
                        infoWindowIndex: id
                    });
+                 }
+        function addMarker2(latLng,time,id,rpm map) {
+                   var marker2 = new google.maps.Marker({
+                       position: latLng,
+                       map: map,
+                       icon: image,
+                       infoWindowIndex: id
+                   });
+                 }
                   // markers.push(marker);
 
                   var content = '<div id="Marker_Time">' +
       '<h6>' + 'Información' + '</h6>' +
       '<p>' + time + '</p>' + '</div>';
+
+      var content2 = '<div id="Marker_Time">' +
+      '<h6>' + 'Información' + '</h6>' +
+      '<p>' + time2 + '</p>' +'<p>' + rpm +'</p>' + '</div>' +
+
 
     var infoWindow = new google.maps.InfoWindow({
       content: content
@@ -96,11 +114,23 @@
                   // infoWindows[this.infoWindowIndex].open(this.map2, this.marker);
                 }
     );
+    var infoWindow2 = new google.maps.InfoWindow({
+      content: content2
+    });
+                  google.maps.event.addListener(marker, 'click',
+                function(event) {
+                  infoWindow.open(map, marker);
+                  // infoWindows[this.infoWindowIndex].open(this.map2, this.marker);
+                }
+    );
 
     infoWindows.push(infoWindow);
+    infoWindows2.push(infoWindow2)
     markers.push(marker);
                    return marker;
-              }
+    markers2.push(marker);
+                   return marker;
+              
         dtpicker='<?=$_POST['datetimepicker']?>';
         dtpicker2='<?=$_POST['datetimepicker2']?>';
         plc='<?=$_POST['plc']?>';
@@ -114,7 +144,7 @@
                    datetimepicker2: dtpicker2,
                    plc: plc
                  },
-                success: function(hist)
+                success: function(hist,hist2)
                 {
 
                     var json_hist = jQuery.parseJSON(JSON.stringify(hist));
@@ -137,6 +167,28 @@
                       myPathTotal2.setPath(myPath)
                       myPathTotal2.setMap(map);
                       addMarker(new google.maps.LatLng(LATITUDE, LONGITUDE),TIME,ID, map);
+
+                    var json_hist2 = jQuery.parseJSON(JSON.stringify(hist2));
+                    initMap();
+                    INIT_LAT2 = parseFloat(json_hist2[json_hist2.length - 1].Latitude);
+                    INIT_LON2 = parseFloat(json_hist2[json_hist2.length - 1].Longitude);
+                    $(json_hist2).each(function() {
+                      var ID2 = this.ID;
+                      var LATITUDE2 = this.Latitude;
+                      var LONGITUDE2 = this.Longitude;
+                      var TIME2 =this.DateTime;
+                      var rpm = this.RPM;
+                      myCoord2b = new google.maps.LatLng(parseFloat(LATITUDE2), parseFloat(LONGITUDE2));
+                      myPath2.push(myCoord2b);
+                      var myPathTotal2b = new google.maps.Polyline({
+                        path: myPath,
+                        strokeColor: '#000',
+                        strokeOpacity: 1.0,
+                        strokeWeight: 5
+                      });
+                      myPathTotal2b.setPath(myPath)
+                      myPathTotal2b.setMap(map);
+                      addMarker2(new google.maps.LatLng(LATITUDE2, LONGITUDE2),TIME2,ID2,rpm, map);
                     });
                 },
                 dataType: "json"//set to JSON
