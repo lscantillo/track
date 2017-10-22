e0d0d430cee3e5c1318a46c3f3c5e78395e9020a
Luis S Cantillo <30596335+lscantillo@users.noreply.github.com>
2017-10-22 12:18:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -209,8 +209,9 @@
 
        <script>
           function move2(){
-              var vector = myPath;
+              var vector[] = myPath;
               vector.toString();
+              document.getElementById("magicbox2").innerHTML = vector;
               var lat2 = parseFloat(myPath[myPath.length - 1].Latitude);
               var lon2 = parseFloat(myPath[myPath.length - 1].Longitude);
                 // var lat2 = "<?php echo $Lat2; ?>";
@@ -230,3 +231,6 @@
           };
 
           </script>
+
+    </body>
+</html>
